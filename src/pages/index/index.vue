110a165268fdb902edf000a15aceff1458c32b34
周周 <1989394260@qq.com>
2024-06-03 17:12:23 +0800

    fix: 项目初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <button @click="increment">watchEffectAAAA</button>
+    <button @click="increment">watchPostEffect</button>
+    <button @click="increment">watchSyncEffect</button>
+	<view class="ass">
+		啊士大夫看见
+	</view>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect, watchPostEffect, watchSyncEffect } from "vue"
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+watchEffect(() => {
+  console.log(`Count is now ${count.value}`,'watchEffect')
+})
+
+watchPostEffect(() => {
+  console.log(`Count is now ${count.value}`,'watchPostEffect')
+})
+
+watchSyncEffect(() => {
+  console.log(`Count is now ${count.value}`,'watchSyncEffect')
+})
+
+
+</script>
